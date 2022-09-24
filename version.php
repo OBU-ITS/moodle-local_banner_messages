44f1c174374435412f2de7620cdd95d51f18e7f7
Jock Coats <p0071665@brookes.ac.uk>
2022-09-24 14:06:25 +0100

    Add a point at which to reset banner_hold variable in lib.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_banner_messages';
-$plugin->release = '0.1.1';
-$plugin->version = 2022092010;
+$plugin->release = '0.1.3';
+$plugin->version = 2022092401;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_ALPHA;
